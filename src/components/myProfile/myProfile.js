209325054c5eea5c0740ab5206ee522b9d263393
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-22 20:14:21 +0000

    Add myProfile repository

---
+++ src/components/myProfile/myProfile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <table>
+    <tr>
+      <th>My Missions</th>
+      <th>My Rockets</th>
+    </tr>
+    <tr className="missions">
+      <td>Falcon 9</td>
+    </tr>
+    <tr className="rockets">
+      <td>Centro comercial Moctezuma</td>
+    </tr>
+  </table>
+);
+
+export default MyProfile;
