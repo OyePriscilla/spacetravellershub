ea48f806637e1a2b4e5613909e2da65d22741f96
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-23 23:55:20 +0000

    Add useSelector to get the fetch rockets from the store and populate the screen

--- src/components/rockets/rockets.js
+++ src/components/rockets/rockets.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockects } from '../../redux/rockets/rockets';
+import Rocket from './rocket';
 
-const Rockets = () => (
-  <div>Rockets Section</div>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocket);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockects());
+  }, []);
+
+  return (
+    <div className="container">
+      {rockets.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          image={rocket.image}
+          name={rocket.name}
+          description={rocket.description}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
