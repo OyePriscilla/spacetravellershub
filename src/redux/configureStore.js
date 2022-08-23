6b824f7c74a18cda6adbd9a4520635050af52b08
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-23 08:20:14 +0000

    Configure the store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rocketsReducer from './rockets/rockets';
+import missionsReducer from './missions/missions';
+
+const rootReducer = combineReducers({
+  rocket: rocketsReducer,
+  mission: missionsReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
+
+export default store;
