84abde4757ee705b9101ee63f21589ae9080e168
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-23 14:07:59 +0100

    Add components

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import Mission from './Mission';
+import MissionHeader from './MissionHeader';
 
-const Missions = () => (
-  <div>Mission Section</div>
-);
+const initialData = [
+  {
+    id: 1,
+    mission: 'Trace',
+    description: 'Lorem ipsum',
+    status: 'active',
+  },
+  {
+    id: 2,
+    mission: 'Check',
+    description: 'Lorem ipsum Hjsjk jkdhjsad djfjdhfe',
+    status: 'passive',
+  },
+  {
+    id: 3,
+    mission: 'Bookddgg',
+    description: 'Lorem ipsum hdghkus hsdhgshd shhjs jnsdjhs hdhgd ',
+    status: 'active',
+  },
+];
+
+function Missions() {
+  return (
+    <div>
+      <MissionHeader />
+      {initialData.map((data) => (
+        <Mission
+          key={data.id}
+          id={data.id}
+          mission={data.mission}
+          description={data.description}
+          statuss={data.status}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default Missions;
