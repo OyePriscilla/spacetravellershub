1ffd0a4cf9eed34f01a3fcc55ce0d020fdbcd1df
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-23 14:05:44 +0100

    Create mission header

---
+++ src/components/missions/MissionHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MissionHeader() {
+  return (
+    <div>
+      <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'space-between' }}>
+        <li>Missions</li>
+        <li>Description</li>
+        <li>Status</li>
+        <li>         </li>
+      </ul>
+    </div>
+  );
+}
