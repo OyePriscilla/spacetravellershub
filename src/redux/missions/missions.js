9bda020dd14011e084564cb660760d6f7c7d6aeb
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-24 10:37:53 +0100

    Change the reserve label to join mission

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const LOADMISSION = 'spacetravellers/missions/LOADMISSION';
+const JOINMISSION = 'spacetravellers/missions/JOINMISSION';
 
 const initialState = [];
 
@@ -9,6 +10,13 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case `${LOADMISSION}/fulfilled`:
       return action.payload.data;
+    case `${JOINMISSION}/fulfilled`: {
+      const newState = state.map((mission) => {
+        if (mission.id !== action.id) { return mission; }
+        return { ...mission, joinmission: true };
+      });
+      return newState;
+    }
     default:
       return state;
   }
@@ -24,4 +32,8 @@ export const fetchApi = createAsyncThunk(LOADMISSION, async () => {
   }));
 
   return { data };
+});
+
+export const JoinMission = (id) => ({
+  type: JOINMISSION, id,
 });
